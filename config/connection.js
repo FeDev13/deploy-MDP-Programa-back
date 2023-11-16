b8f6a93861d14dd86f9e04c76bbd2381ee59aae6
Federico Robledo <federicorobledo85@gmail.com>
2023-11-15 22:04:23 -0300

    cambia datos de conexion

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("usuarios", "root", "", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  "bshqvmjmuipphzzp14gz",
+  "ruruqycihtc2ai6du",
+  "7DXq4R012vadTZL32DAc",
+  {
+    dialect: "mysql",
+    host: "bshqvmjmuipphzzp14gz-mysql.services.clever-cloud.com",
+  }
+);
 
 const connectDB = async () => {
   try {
